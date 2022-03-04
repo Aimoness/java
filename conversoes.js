749bd0c50aaac28eb9b7931eee6e5a346b75887d
Aimone Silva <silvaaimone@gmail.com>
2022-03-03 21:05:44 -0300

    Codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipode de dado
+// booleanos
+
+// conersão implícita
+const numero = 456;
+const numeroString = Number("456");
+
+//Number()
+//String()
+console.log(numero + numeroString)
+
+
+
+//conversaõ explícita
